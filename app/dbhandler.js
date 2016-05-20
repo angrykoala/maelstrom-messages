aa264cbeb86327c40da47171b7f4e95a6a453ec3
demiurgosoft <demiurgosoft@hotmail.com>
2016-05-20 16:28:44 +0200

    dbhandler layout

--- app/dbhandler.js
+++ app/dbhandler.js
@@ -2,11 +2,19 @@ var User=require('./models/user');
 var mongoose=require('mongoose');
 
 var dbHandler={
+    login: function(userId,done){
+        
+
+    },
     getInbox: function(userId,done){
 
 
         done();
-    }
+    },
+    getInboxMessage: function(userId,msgId,done){
+
+
+    },
 };
 
 module.exports=dbHandler;
